a03c980bd8e9c9adec5403e1b8a76e1a10b4e6f3
Matias <matias_arenhard@hotmail.com>
2022-02-15 22:31:42 -0300

    add onclick event

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const handleMinusClick = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }; 
 
   return (
   <>
       <h1>Contador: {count}</h1>
       
-    <button>+</button>
+      <button
+        className="minus"
+        onClick={handleMinusClick}
+      >
+        -
+      </button>
+
+      <button className="plus" onClick={() => setCount(count + 1)}>+</button>
   </>
   );
 }
